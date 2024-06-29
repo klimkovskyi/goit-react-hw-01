5390d07a79acd4693ba9dd006bbcff5679a0f816
Andrii Klimkovskyi <andrew.klimkovskyi@gmail.com>
2024-06-29 12:49:21 +0300

    create template react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import userData from "./userData.json";
+import Profile from "./components/Profile/Profile";
+
+function App() {
+  const { username, tag, location, avatar, stats } = userData;
+
+  return (
+    <>
+      <Profile
+        name={username}
+        tag={tag}
+        location={location}
+        image={avatar}
+        stats={stats}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+// import css from "./Profile.module.css";
+
+const Profile = ({
+  name,
+  tag,
+  location,
+  image,
+  stats: { folowers, views, likes },
+}) => {
+  return (
+    <div>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{folowers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
